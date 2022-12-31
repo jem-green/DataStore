e5ba405a9adff415f1318e7022d134a0d6764603
Jeremy Green <jem.green@outlook.com>
2022-12-31 22:39:27 +0000

    Add project files.

---
+++ DataStoreConsole/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using StorageLibrary;
+using System.CommandLine;
+using System.CommandLine.Parser;
+using System.CommandLine.Invocation;
+
+namespace StorageConsole
+{
+    /// <summary>
+    /// Simple command line to return results
+    /// More like the docker command line so
+    /// not a query language just parameters
+    /// </summary>
+    class Program
+    {
+        // Need the name and location of the file to read
+        // could pass this as a single parameter if nothing
+        // else is supplied
+        // so --name --path
+        // and log to the console all the data in cvs format
+
+        #region Fields
+
+        static string _name = "store";
+        static string _path = "";
+        bool reset;
+        static PersistentDataStore _dataStore;
+
+        #endregion
+
+        static int Main(string[] args)
+        {
+            var root = new RootCommand()
+            {
+                 new Option<string>(new string[] { "--name", "-N" }, description: "The data store name"),
+                 new Option<string>(new string[] { "--path", "-P" }, description: "The data stre path"),
+                 new Option<bool>(new string[] { "--reset", "-R" }, description: "Reset the data store"),
+            };
+            root.Handler = CommandHandler.Create<string, string, bool>(HandleRoot);
+
+            Command set = new Command("set", "Set field")
+            {
+                new Option<string>(new string[] { "--name", "-n" }, description: "The field name"),
+                new Option<string>(new string[] { "--type", "-T" }, description: "The field type"),
+                new Option<int>(new string[] { "--length", "-L" }, description: "The string field length"),
+            };
+            set.Handler = CommandHandler.Create<string,string,sbyte>(HandleSet);
+
+            Command delete = new Command("delete", "Delete record by index")
+            {
+                new Argument<int>("index","Record index")
+            };
+            delete.Handler = CommandHandler.Create<int>(HandleDelete);
+
+            Command update = new Command("update", "Update record by index")
+            {
+                new Option<int>(new string[] { "--row", "-R" }, description: "The record row"),
+                new Option(new string[] { "--all", "-A" }, description: "All records"),
+            };
+            update.Handler = CommandHandler.Update<int, object>(HandleUpdate);
+
+            Command create = new Command("create", "Create new record")
+            {
+                new Option<int>(new string[] { "--row", "-R" }, description: "The record row"),
+                new Option(new string[] { "--all", "-A" }, description: "All records"),
+            };
+            create.Handler = CommandHandler.Create<int, object>(HandleCreate);
+
+            root.Add(set);
+            root.Add(delete);
+            root.Add(update);
+            root.Add(create);
+
+            return root.Invoke(args);
+        }
+
+        private static void HandleSet(string name, string type, sbyte length)
+        {
+            // Assume that the datastore has been opened
+            // Assume we can add the fields in order
+
+            if (_dataStore != null)
+            {
+                int item = _dataStore.Length;
+                item++;
+                _dataStore.Set(item, name, type, length);
+            }
+        }
+
+        private static void HandleGet(int item)
+        {
+            // Assume that the datastore has been opened
+            if (_dataStore != null)
+            {
+                _dataStore.Get(item);
+            }
+        }
+
+        private static void HandleRoot(string name, string path, bool reset)
+        {
+            if (name != "")
+            {
+                _name = name;
+            }
+            if (path != "")
+            {
+                _path = path;
+            }
+            _dataStore = new PersistentDataStore(_path,_name, reset);
+        }
+
+        private static void HandleCreate(int row, object data)
+        {
+            // Assume that the datastore has been opened
+            if (_dataStore != null)
+            {
+                string[] data = new string[_dataStore.Length];
+                _dataStore.Create(data, row);
+            }
+        }
+
+        private static string HandleRead(int row, bool all)
+        {
+            // Assume that the datastore has been opened
+            // Need to build the list of fields to form a record
+            // The list must be in order
+
+            string data = "";
+            if (_dataStore != null)
+            {
+                data = _dataStore.Read(row, all);
+            }
+            return(data);
+        }
+
+        private static void HandleDelete(int row, bool all)
+        {
+            // Assume that the datastore has been opened
+            if (_dataStore != null)
+            {
+                _dataStore.Delete(row, all);
+            }
+        }
+
+        private static void HandleUpdate(int row, object data)
+        {
+            // Assume that the datastore has been opened
+            // Need to build the list of fields to form a record
+            // The list must be in order
+
+            if (_dataStore != null)
+            {
+                string[] data = new string[_dataStore.Length];
+                _dataStore.Update(data, row);
+            }
+        }
+
+    }
+}
---
+++ DataStoreLibrary/PersistentDataStore.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace StorageLibrary
+{
+    public class PersistentDataStore
+    {
+        #region Fields
+
+        private string _path = "";
+        private string _name = "Storage";
+        private DataHandler _handler;
+
+        public struct FieldType
+        {
+            string _name;
+            string _type;
+            sbyte _length;
+
+            internal FieldType(string name, string type, sbyte length)
+            {
+                _name = name;
+                _type = type;
+                _length = length;
+            }
+
+            internal sbyte Length
+            {
+                set
+                {
+                    _length = value;
+                }
+                get
+                {
+                    return (_length);
+                }
+            }
+
+            internal string Name
+            {
+                set
+                {
+                    _name = value;
+                }
+                get
+                {
+                    return (_name);
+                }
+            }
+
+
+            internal string Type
+            {
+                set
+                {
+                    _type = value;
+                }
+                get
+                {
+                    return (_type);
+                }
+            }
+
+            public override string ToString()
+            {
+                string s = _name + "," + _type;
+                if (_length > 0)
+                {
+                    s = s + "[" + _length + "]";
+                }
+                return (s);
+            }
+        }
+
+        #endregion
+        #region Constructors 
+
+        /// <summary>
+        /// Open or create a new data store
+        /// </summary>
+        public PersistentDataStore()
+        {
+            // Reset, Open or create a new store based on the type
+
+            _handler = new DataHandler(_path, _name);
+            if (_handler.Open() == false)
+            {
+                _handler.Reset();
+            }
+        }
+
+        /// <summary>
+        /// Reset, Open or create a new store based on the type
+        /// </summary>
+        /// <param name="reset"></param>
+        public PersistentDataStore(bool reset)
+        {
+            // Reset, Open or create a new store based on the type
+
+            _handler = new DataHandler(_path, _name);
+            if ((_handler.Open() == false) || (reset == true))
+            {
+                _handler.Reset();
+            }
+        }
+
+        /// <summary>
+        /// Reset, Open or create a new store based on the type with a specific name and location
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="filename"></param>
+        /// <param name="reset"></param>
+        public PersistentDataStore(string path, string filename, bool reset)
+        {
+            _path = path;
+            _name = filename;
+
+            _handler = new DataHandler(_path, _name);
+            if ((_handler.Open() == false) || (reset == true))
+            {
+                _handler.Reset();
+            }
+        }
+
+        #endregion
+        #region Properties
+
+        public int Size
+        {
+            get
+            {
+                return (_handler.Size);
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return (_handler.Fields.Length);
+            }
+        }
+
+        #endregion
+        #region Methods
+
+        public void Add(string name, string type, sbyte length)
+        {
+            if (_handler != null)
+            {
+                DataHandler.Property field = new DataHandler.Property();
+                field.Name = name;
+                field.Type = TypeLookup(type);
+                if (field.Type == TypeCode.String)
+                {
+                    field.Length = length;
+                }
+                _handler.Add(field);
+            }
+        }
+
+        public void Remove(int item)
+        {
+            if (_handler != null)
+            {
+                _handler.RemoveAt(item);
+            }
+        }
+
+        public void Set(int item, string name, string type, sbyte length)
+        {
+            if (_handler != null)
+            {
+                DataHandler.Property field = new DataHandler.Property();
+                field.Name = name;
+                field.Type = TypeLookup(type);
+                if (field.Type == TypeCode.String)
+                {
+                    field.Length = length;
+                }
+                _handler.Set(field, item);
+            }
+        }
+
+        public void Set(int item, FieldType fieldType)
+        {
+            if (_handler != null)
+            {
+                DataHandler.Property field = new DataHandler.Property();
+                field.Name = fieldType.Name;
+                field.Type = TypeLookup(fieldType.Type);
+                if (field.Type == TypeCode.String)
+                {
+                    field.Length = fieldType.Length;
+                }
+                _handler.Set(field, item);
+            }
+        }
+
+        // WHat do we return here an object / stuc with the field structure
+
+        public FieldType Get(int item)
+        {
+            FieldType fieldType = new FieldType();
+
+            if (_handler != null)
+            {
+                DataHandler.Property property = new DataHandler.Property();
+                property = _handler.Get(item);
+                fieldType.Name = property.Name;
+                fieldType.Type = property.Type.ToString();
+                if (property.Type == TypeCode.String)
+                {
+                    property.Length = property.Length;
+                }
+            }
+            return (fieldType);
+        }
+
+
+        /// <summary>
+        /// Delete the data at index
+        /// </summary>
+        /// <param name="row"></param>
+        public void Delete(int row, bool all)
+        {
+            if (_handler != null)
+            {
+                if ((row >= 0) && (row <= _handler.Size))    // Inital check to save processing
+                {
+                    if (all == true)
+                    {
+                        _handler.Delete(row,all);
+                    }
+                    else
+                    {
+                        _handler.Delete(row);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creae new record
+        /// </summary>
+        /// <param name="data"></param>
+        public void Create(object[] data)
+        {
+            if (_handler != null)
+            {
+                _handler.Create(data);
+            }
+        }
+
+        /// <summary>
+        /// Creae new record
+        /// </summary>
+        /// <param name="data"></param>
+        public void Create(object[] data)
+        {
+            if (_handler != null)
+            {
+                _handler.Create(data);
+            }
+        }
+
+        /// <summary>
+        /// Read record at row
+        /// </summary>
+        /// <param name="row"></param>
+        public string Read(int row, bool all)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (_handler != null)
+            {
+                if (all == true)
+                {
+                    for (int i = 0; i < _handler.Size; i++)
+                    {
+                        object[] data;
+                        data = _handler.Read(i);
+                        for (int j = 0; j < data.Length; j++)
+                        {
+                            // this will depend on the output formal which needs
+                            // passing in via the command line but assume csv
+                            builder.Append(_handler.Fields[j]);
+                            builder.Append(",");
+                            switch (_handler.Fields[j].Type)
+                            {
+                                case TypeCode.Int16:
+                                    {
+                                        builder.Append(data[j]);
+                                        break;
+                                    }
+                                case TypeCode.String:
+                                    {
+                                        builder.Append("\"");
+                                        builder.Append(data[j]);
+                                        builder.Append("\"");
+                                        break;
+                                    }
+                            }
+                        }
+                        builder.Append("\n");
+                    }
+                }
+                else
+                {
+                    object[] data;
+                    data = _handler.Read(row);
+                    for (int j = 0; j < data.Length; j++)
+                    {
+                        // this will depend on the output format which needs
+                        // passing in via the command line but assume csv
+                        builder.Append(_handler.Fields[j]);
+                        builder.Append(",");
+                        switch (_handler.Fields[j].Type)
+                        {
+                            case TypeCode.Int16:
+                                {
+                                    builder.Append(data[j]);
+                                    break;
+                                }
+                            case TypeCode.String:
+                                {
+                                    builder.Append("\"");
+                                    builder.Append(data[j]);
+                                    builder.Append("\"");
+                                    break;
+                                }
+                        }
+                    }
+                }
+                
+            }
+            return (builder.ToString());
+        }
+
+
+        /// <summary>
+        /// Update the data at index
+        /// </summary>
+        /// <param name="row"></param>
+        public void Update(string kvp, int row)
+        {
+            if (_handler != null)
+            {
+                if ((row >= 0) && (row <= _handler.Size))    // Inital check to save processing
+                {
+                    object[] data = new object[_handler.Fields.Length];
+                    _handler.Update(data, row);
+                }
+            }
+        }
+
+        #endregion
+        #region Private
+
+        private TypeCode TypeLookup(string type)
+        {
+            TypeCode dataType = TypeCode.Int16;
+
+            switch (type.ToUpper())
+            {
+                case "I":
+                case "INT":
+                    dataType = TypeCode.Int16;
+                    break;
+                case "S":
+                case "STRING":
+                    dataType = TypeCode.String;
+                    break;
+            }
+            return (dataType);
+        }
+
+        #endregion
+    }
+}
